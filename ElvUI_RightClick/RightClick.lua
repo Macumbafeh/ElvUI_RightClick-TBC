9fc582474ce299aafe5953229bebe6e7eac2c32a
Crum <stschleyer89@gmail.com>
2017-08-29 17:56:50 -0500

    update TBC

--- ElvUI_RightClick/RightClick.lua
+++ ElvUI_RightClick/RightClick.lua
@@ -1,10 +1,9 @@
-local AB = ElvUI[1]:GetModule("ActionBars");
+local AB = ElvUI[1]:GetModule("ActionBars")
 
-local AddOnName = ...
-LibStub("LibElvUIPlugin-1.0"):RegisterPlugin(AddOnName)
+LibStub("LibElvUIPlugin-1.0"):RegisterPlugin("ElvUI_RightClick")
 
 hooksecurefunc(AB, "UpdateButtonConfig", function(_, bar)
 	for _, button in pairs(bar.buttons) do
-		button:SetAttribute("unit2", "player");
+		button:SetAttribute("unit2", "player")
 	end
-end);
+end)
