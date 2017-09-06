f34dd9a79b2397bf1e204ad5e0fba82a39d004aa
q3fuba <q3fuba@gmail.com>
2017-09-06 16:44:49 +0200

    Update Actiobars at Login to make it work properly

--- ElvUI_RightClick/RightClick.lua
+++ ElvUI_RightClick/RightClick.lua
@@ -1,7 +1,16 @@
 local AB = ElvUI[1]:GetModule("ActionBars")
-
 LibStub("LibElvUIPlugin-1.0"):RegisterPlugin("ElvUI_RightClick")
 
 hooksecurefunc(AB, "UpdateButtonConfig", function(_, bar)
 	bar:SetAttribute("unit-S"..(bar:GetAttribute("state-page") or 0).."Right", "player")
+end)
+
+-- Update Actiobars at Login to make it work properly - by fuba
+local f = CreateFrame("FRAME")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", function(...)
+	if event == "PLAYER_ENTERING_WORLD" then
+		AB:UpdateButtonSettings()
+		f:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	end
 end)
